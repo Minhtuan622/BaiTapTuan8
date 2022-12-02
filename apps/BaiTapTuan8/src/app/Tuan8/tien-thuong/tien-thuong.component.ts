8e88a6272218df78eb26601c1921439e5b49a7cf
Minhtuan622 <minhtuannguyen22062001@gmail.com>
2022-12-02 11:52:12 +0700

    add file to github

--- apps/BaiTapTuan8/src/app/Tuan8/tien-thuong/tien-thuong.component.ts
+++ apps/BaiTapTuan8/src/app/Tuan8/tien-thuong/tien-thuong.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface loai {
+  id: string,
+  name: string
+}
+
 @Component({
   selector: 'app-tien-thuong',
   templateUrl: './tien-thuong.component.html',
@@ -7,13 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TienThuongComponent implements OnInit {
   public luong: number = 0;
-  public thuong: number = 0;
+  public hesothuong: number = 0;
+  public tienthuong: number = 0;
+  public xeploai: string = "";
+  public loai: loai[] = [
+    {id: '0', name: 'Chọn Loại'},
+    {id: 'A', name: 'Loại A'},
+    {id: 'B', name: 'Loại B'},
+    {id: 'C', name: 'Loại C'},
+    {id: 'D', name: 'Loại D'}
+  ];
 
-  private hesothuong(){
-    if ()
+  public chonLoai(event: any){
+    this.xeploai = event.target.value;
+    switch (this.xeploai) {
+      case 'A':
+        this.hesothuong = 2;
+        break;
+      case 'B':
+        this.hesothuong = 1.8;
+        break;
+      case 'C':
+        this.hesothuong = 1.2;
+        break;
+      case 'D':
+        this.hesothuong = 0.8;
+        break;
+    }
+  }
+
+  public tinhtien(){
+    this.tienthuong = this.luong * this.hesothuong;
   }
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
